fb54f5c5b06608f5abfd7d8f64a1796d6e6e9ab5
yangmingtai <961612727@qq.com>
2017-03-15 10:20:12 +0800

    add mail

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.5
-from flask.ext.script import Manager
+import os
+from flask.ext.script import Manager, Shell
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.moment import Moment
 # from datetime import datetime
@@ -8,6 +9,10 @@ from flask import Flask, render_template, session, redirect, url_for
 from flask.ext.wtf import Form
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
+from flask.ext.mail import Mail
+from flask.ext.sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class NameForm(Form):
@@ -19,16 +24,69 @@ app = Flask(__name__)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+
+# database config
 app.config['SECRET_KEY'] = 'ymt origin csuft comeon'
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# mail config
+app.config['MAIL_SERVER'] = 'mx1.qq.com'
+app.config['MAIL_PORT'] = '465'
+app.config['MAIL_USE_TSL'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWPRD'] = os.environ.get('MAIL_PASSWORD')
+
+mail = Mail(app)
+db = SQLAlchemy(app)
+
+
+class Role(db.Model):
+    __tabename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('role.id'))
+
+    def __repr__(self):
+        return '<User %r>' % self.username
+
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User, Role=Role)
+
+
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
         session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'),
+                           known=session.get('known', False))
 
 
 @app.route('/user/<name>')
@@ -41,7 +99,7 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 
-@app.errorhandler(500)
+@app.errorhandler(501)
 def internal_server_error(e):
     return render_template('500.html'), 500
 
