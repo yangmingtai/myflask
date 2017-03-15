2d5650ede693fdf0ea4525c1db99c55e999391e1
yangmingtai <961612727@qq.com>
2017-03-15 22:15:58 +0800

    new life

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     MAIL_SERVER = 'smtp.163.com'
     MAIL_PORT = 465
-    MAIL_USE_TLS = True
+    MAIL_USE_TSL = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Origin]'
